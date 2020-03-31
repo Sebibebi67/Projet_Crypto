51d68be8e7bcf4134c50a7de785ffc98c6295b69
Sebibebi67 <sebibebi67@gmail.com>
2020-03-31 15:13:02 +0200

    adding euclide algorithms

--- Euclide.py
+++ Euclide.py
@@ -6,17 +6,39 @@ def simpleIt(a, b):
 
 
 def simpleRec(a, b):
-    r= a%b
+    r = a%b
     if r!=0:
         return simpleRec(b, a%b)
     else:
         return b
     
 
-# def etenduIt(a, b):
+def etenduIt(a, b):
+    (r0,r1) = (a, b)
+    (u0, u1) = (1, 0)
+    (v0, v1) = (0, 1)
 
+    while r1!=0:
+        q = r0//r1
+        (r0, r1) = (r1, r0 - q * r1)
+        (u0, u1) = (u1, u0 - q * u1)
+        (v0, v1) = (v1, v0 - q * v1)
+    return r0, u0, v0
 
-# def etenduRec(a, b):
+def etenduRecInit(a, b):
+    return etenduRec(a, b, 1, 0, 0, 1)
+
+def etenduRec(a, b, u0, u1, v0, v1):
+    (r0,r1) = (a, b)
+
+    if r1!=0:
+        q = r0//r1
+        (r0, r1) = (r1, r0 - q * r1)
+        (u0, u1) = (u1, u0 - q * u1)
+        (v0, v1) = (v1, v0 - q * v1)
+        return etenduRec(r0, r1, u0, u1, v0, v1)
+    else:
+        return r0, u0, v0
     
 
 
--- test.py
+++ test.py
@@ -10,9 +10,16 @@ import Euclide
 
 
 
-a = Euclide.simpleIt(2, 4)
-b = Euclide.simpleRec(2, 4)
-print(a, b)
+# a = Euclide.simpleIt(47997435, 355714785)
+# b = Euclide.simpleRec(47997435, 355714785)
+# print(a, b)
+
+
+(i, j) = (45, 63)
+(x, y, z) = Euclide.etenduIt(i, j)
+(x2, y2, z2) = Euclide.etenduRecInit(i, j)
+print(x, y*i+j*z)
+print(x2, y2*i+j*z2)
 
 
 
